f0e94af39411e675e1805398cae66f8a47fe9e01
punchsppt <supapit.r@mail.kmutt.ac.th>
2025-04-01 16:35:18 +0700

    Try to update import path

--- manage-mart-backend/src/controllers/paymentMethodController.js
+++ manage-mart-backend/src/controllers/paymentMethodController.js
@@ -1,26 +1,25 @@
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
-const modelPath = path.resolve(__dirname, "../models/paymentMethod.js");
-console.log("Checking model path:", modelPath);
-console.log("File exists?", fs.existsSync(modelPath));
+// const modelPath = path.resolve(__dirname, "../models/paymentMethod.js");
+// console.log("Checking model path:", modelPath);
+// console.log("File exists?", fs.existsSync(modelPath));
 
-const paymentMethod = require("../models/paymentMethod");
+// const paymentMethod = require("../models/paymentMethod");
 
+// const getAllPaymentMethods = async (req, res) => {
+//   try {
+//     const paymentMethods = await paymentMethod.findAll({
+//         order: [
+//             ['paymentMethodId', 'ASC']
+//         ]
+//     });
+//     res.status(200).json(paymentMethods);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-const getAllPaymentMethods = async (req, res) => {
-  try {
-    const paymentMethods = await paymentMethod.findAll({
-        order: [
-            ['paymentMethodId', 'ASC']
-        ]
-    });
-    res.status(200).json(paymentMethods);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-module.exports = {
-  getAllPaymentMethods,
-};
+// module.exports = {
+//   getAllPaymentMethods,
+// };
