4ab9337be73d75bcaf1566d0d4d42881126e8276
punchsppt <supapit.r@mail.kmutt.ac.th>
2025-04-01 15:54:53 +0700

    Debugging paymentMethod.js import issue

--- manage-mart-backend/src/controllers/paymentMethodController.js
+++ manage-mart-backend/src/controllers/paymentMethodController.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const modelPath = path.resolve(__dirname, "../../models/paymentMethod.js");
+console.log("Checking model path:", modelPath);
+console.log("File exists?", fs.existsSync(modelPath));
+
 const paymentMethod = require("../../models/paymentMethod");
 
 const getAllPaymentMethods = async (req, res) => {
